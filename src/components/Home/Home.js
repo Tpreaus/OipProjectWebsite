cfb9751d956f148fe4df504f9141d3704aaecb80
Theodore Preaus <Preaus6@gmail.com>
2023-12-08 13:36:45 +0100

    starting but

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+/* 
+Author : Theo Preaus
+
+Create a basic website with 2 tabs Approved Class / Blogs and link to PostgreSQL database
+
+*/
+function Home() {
+    return (
+        <div className="Home">
+            
+        </div>
+    )
+}
