c0a3f7642108f698cffe289c11826de5f338d30a
Theodore Preaus <Preaus6@gmail.com>
2023-12-10 11:04:48 +0100

    Added Rating

--- src/components/BasicRating.js
+++ src/components/BasicRating.js
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
 
 export default function BasicRating() {
+    const [value, setValue] = React.useState(2);
+
+
   return (
-    <Box>
-      <Rating name="read-only" value={3} readOnly />
+    <Box sx={{'& > legend': { mt: 2 },}}>   
+      <Rating
+        name="simple-controlled"
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      />
     </Box>
   );
 }
--- src/components/Home/Intro/Blogdemo/Blogdemo.js
+++ src/components/Home/Intro/Blogdemo/Blogdemo.js
@@ -1,4 +1,5 @@
 import BasicRating from "../../../BasicRating"
+import HoverRating from "../../../HoverRating"
 import "./Blogdemo.css"
 import blog1 from "./blog1.png"
 import blog2 from "./blog2.png"
@@ -10,16 +11,19 @@ function Blogdemo() {
             <div className="blogsquarecontainer">
                 <div className="blogsquare">
                     <img src={blog1} className="blogimage"/>
+                    <BasicRating/>
                     <h2>My Time in Ifrane</h2>
                     <p>While exploring the wonders of Aui ...</p>
                 </div>
                 <div className="blogsquare">
                     <img src={blog2} className="blogimage"/>
+                    <BasicRating/>
                     <h2>Exploring The Blue City of Morocco</h2>
                     <p>This city really was something else ...</p>
                 </div>
                 <div className="blogsquare">
                     <img src={blog3} className="blogimage"/>
+                    <BasicRating/>
                     <h2>The Dessert</h2>
                     <p>I have sand everywhere ...</p>
                 </div>
