78442b27a12f883c6e355d3f8ca541d264247185
Theodore Preaus <Preaus6@gmail.com>
2023-12-12 21:37:50 +0100

    SAVE BEFORE STARTING VIEW

--- src/components/CourseDisp/CourseDisp.js
+++ src/components/CourseDisp/CourseDisp.js
@@ -1,9 +1,42 @@
-import "./CourseDisp.js"
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+
 
 export default function CourseDisp() {
-    return(
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/courses')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Data:', data);
+                setCourses(data);
+            })
+            .catch(error => console.error('Error:', error));
+    }, []);
+
+    return (
         <div className="coursedisp">
-            Check
+            {courses.map(course => (
+                <div key={course.approvedclassid}>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="body2" component="div">
+                                AUI Course ID - {course.auicourseid}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Course Code at Host - {course.hostcourseid}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </div>
+            ))}
         </div>
-    )
+    );
+    
 }
